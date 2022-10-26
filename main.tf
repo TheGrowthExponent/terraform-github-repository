92590f8ff7431a2c99b3a1641cf68dbe778e2e6d
Jason Pascoe <jason.pascoe@fmgl.com.au>
2022-10-26 08:53:40 +0800

    use branch protection 3

--- main.tf
+++ main.tf
@@ -181,7 +181,7 @@ resource "github_branch_default" "default" {
 # https://registry.terraform.io/providers/integrations/github/latest/docs/resources/branch_protection_v3
 # ---------------------------------------------------------------------------------------------------------------------
 
-resource "github_branch_protection" "branch_protection" {
+resource "github_branch_protection_v3" "branch_protection" {
   count = length(local.branch_protections)
 
   # ensure we have all members and collaborators added before applying
@@ -193,8 +193,8 @@ resource "github_branch_protection" "branch_protection" {
     github_branch.branch,
   ]
 
-  repository_id                   = github_repository.repository.name
-  pattern                         = local.branch_protections[count.index].branch
+  repository                      = github_repository.repository.name
+  branch                          = local.branch_protections[count.index].branch
   enforce_admins                  = local.branch_protections[count.index].enforce_admins
   require_conversation_resolution = local.branch_protections[count.index].require_conversation_resolution
   require_signed_commits          = local.branch_protections[count.index].require_signed_commits
@@ -213,15 +213,22 @@ resource "github_branch_protection" "branch_protection" {
 
     content {
       dismiss_stale_reviews           = required_pull_request_reviews.value.dismiss_stale_reviews
-      restrict_dismissals             = true
-      dismissal_restrictions          = required_pull_request_reviews.value.dismissal_users
-      pull_request_bypassers          = required_pull_request_reviews.value.dismissal_users
+      dismissal_users                 = required_pull_request_reviews.value.dismissal_users
+      dismissal_teams                 = [for t in required_pull_request_reviews.value.dismissal_teams : replace(lower(t), "/[^a-z0-9_]/", "-")]
       require_code_owner_reviews      = required_pull_request_reviews.value.require_code_owner_reviews
       required_approving_review_count = required_pull_request_reviews.value.required_approving_review_count
     }
   }
 
-  push_restrictions = local.team_push
+  dynamic "restrictions" {
+    for_each = local.restrictions[count.index]
+
+    content {
+      users = restrictions.value.users
+      teams = [for t in restrictions.value.teams : replace(lower(t), "/[^a-z0-9_]/", "-")]
+      apps  = restrictions.value.apps
+    }
+  }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
